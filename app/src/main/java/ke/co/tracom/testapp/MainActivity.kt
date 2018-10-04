d970374f562c8883c809265112a38487c7b36b4b
Kennedy Kariuki <kkariuki@tracom.co.ke>
2018-10-04 16:41:42 +0300

    Initial Commit

---
+++ app/src/main/java/ke/co/tracom/testapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package ke.co.tracom.testapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
